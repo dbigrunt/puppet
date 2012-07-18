d693384140cd256e2fe2cbc57ba7b5e034662369
Xavi Carrillo <xavi.carrillo@gmail.com>
2012-07-18 13:53:09 +0200

    Add Clamav module

--- manifests/nodes/falcon.jcbconsulting.biz.pp
+++ manifests/nodes/falcon.jcbconsulting.biz.pp
@@ -1,13 +1,7 @@
 node 'falcon.jcbconsulting.biz' inherits base {
     #import profile
     #include vim, syslog-ng
-
-    file { "/var/log/clamav/":
-        owner  => 'clamav',
-        group  => 'clamav',
-        mode   => 775,
-		ensure => directory,
-		recurse => true,
-    }
+	import "clamav"
+	include clamav
 
 }
---
+++ modules/clamav/manifests/classes/clamav.pp
@@ -0,0 +1,52 @@
+class clamav {
+
+	package { ["clamav","clamd","clamav-db"]:
+		ensure   => installed,
+		before   => Service["clamd"],
+		provider => "yum"
+	}
+
+	service { "clamd":
+		ensure    => running,
+		enable    => true,
+        hasstatus => true,
+        require   => Package["clamd"]
+	}
+
+    file { "/var/log/clamav/":
+        owner   => 'clamav',
+        group   => 'clamav',
+        mode    => 775,
+		ensure  => directory,
+		recurse => true,
+    }
+
+    file { "/var/lib/clamav/":
+        owner   => 'clam',
+        group   => 'clamav',
+        mode    => 775,
+        ensure  => directory,
+        recurse => false,
+    }
+
+    user { clam:
+        comment => "Clam Anti Virus Checker",
+        home    => "/var/clamav",
+        shell   => "/sbin/nologin",
+        #uid     => 118,
+		groups  => "clam",
+    }
+
+    user { clamav:
+        comment => "Clam Anti Virus Checker",
+        home    => "/var/clamav",
+        shell   => "/sbin/nologin",
+        #uid     => 115,
+        groups  => "clam",
+    }
+
+    group { clam:
+        #gid     => 118,
+        require => user[clam],
+    }
+}
