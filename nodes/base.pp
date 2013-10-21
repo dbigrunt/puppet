0dfd0018129ccbb0c1fab400013b192b00c618b8
Xavi Carrillo <xavi.carrillo@gmail.com>
2013-10-21 15:30:09 +0200

    Refactor base node

--- nodes/base.pp
+++ nodes/base.pp
@@ -1,26 +1,25 @@
 node base {
 
-    $puppetserver = "mundodisea.com"
-    $resolv_searchpaths = ['jcbconsulting.biz']
-    $resolv_nameservers = ['127.0.0.1','8.8.8.8','8.8.4.4']
-    $resolv_options     = ['rotate','timeout:1','attempts:5']
+  $puppetserver       = 'mundodisea.com'
+  $resolv_searchpaths = ['jcbconsulting.biz']
+  $resolv_nameservers = ['127.0.0.1','8.8.8.8','8.8.4.4']
+  $resolv_options     = ['rotate','timeout:1','attempts:5']
 
-    import "resolv"
-    resolv_conf { "basenode_resolv":
-        searchpath  => $resolv_searchpaths,
-        nameservers => $resolv_nameservers,
-        options     => $resolv_options,
-    }
+  import 'resolv'
+  resolv_conf { 'basenode_resolv':
+    searchpath  => $resolv_searchpaths,
+    nameservers => $resolv_nameservers,
+    options     => $resolv_options,
+  }
 
-    #import "profile"
-    include profile
-    import "ntp"
-    include ntp-client
-    include puppet
+  import 'profile'
+  include profile
+  import 'ntp'
+  include ntp-client
+  include puppet
 
-    file { "/tmp":
-        ensure => directory,
-        mode => 1777,
-    }
-
+  file { '/tmp':
+      ensure => directory,
+      mode   => 1777,
+  }
 }
