ce274becee2c076ab6dad76cae6d1b5c87d830e2
root <root@vps38933.ovh.net>
2013-11-19 18:03:20 +0100

    Make the ntp more standard. Move site.pp to its default location

--- manifests/nodes/vps38933.ovh.net.pp
+++ manifests/nodes/vps38933.ovh.net.pp
@@ -1,13 +1,13 @@
 node 'vps38933.ovh.net' inherits default {
 
   $administrator_email       = 'xavi.carrillo@gmail.com'
-  $puppet_fileserver_allowed = [ '*.jcbconsulting.biz', 'vps38933.ovh.com' ] 
+  $puppet_fileserver_allowed = [ '*.jcbconsulting.biz', 'vps38933.ovh.net' ] 
   $rkhunter_xinetd_allowed   = [ 'echo','ftp_psa','poppassd_psa','smtp_psa','smtps_psa', 'submission_psa' ]
   $rkhunter_disable_tests    = [ 'suspscan hidden_procs deleted_files packet_cap_apps apps os_specific' ]
   # os_specific: related to kernel modules, which we don't use
 
   include puppet::master
-  import 'clamav'
+  #import 'clamav'
   include clamav
   clamav::scan { "$name":
     directory => "/var/www/vhosts",
--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -1,1 +1,36 @@
-import "classes/*"
+
+class ntp ($servers = [ '0.centos.pool.ntp.org' ],) {
+
+  case $::osfamily{
+    'Debian':  {
+      $service_name = 'ntp'
+    }
+    'RedHat':  {
+      $service_name = 'ntpd'
+    }
+    default: {
+      fail("Unsupported platform: ${::operatingsystem}")
+    }
+  }
+
+  $packages = 'ntp'
+  package { $packages: ensure => 'latest' }
+
+  service { $service_name:
+    ensure  => 'running',
+    enable  => true,
+    require => Package['ntp'],
+  }
+
+  file { '/etc/ntp.conf':
+    require => Package['ntp'],
+    notify  => Service[$service_name],
+    content => template('ntp/ntp.conf.erb'),
+  }
+
+  file { '/etc/logrotate.d/ntpd':
+    source => 'puppet:///ntp/logrotate.d/ntpd',
+  }
+
+}
+
