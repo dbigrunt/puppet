ca52749c40facd9349ea727bf39a180e6dc94456
Xavi Carrillo <xavi.carrillo@gmail.com>
2013-11-19 16:09:50 +0100

    Make this module autoloadable by putting the main class into init.pp

--- modules/profile/manifests/init.pp
+++ modules/profile/manifests/init.pp
@@ -1,1 +1,12 @@
-import "classes/*"
+class profile {
+  file { '/etc/profile.d':
+    ensure  => directory,
+    source  => 'puppet:///files/etc/profile.d',
+    recurse => false,
+    purge   => false,
+    ignore  => ['.svn','svn'],
+    owner   => 'root',
+    group   => 'root',
+  }
+}
+
