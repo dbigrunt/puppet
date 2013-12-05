01035b3722706cc1877962c34e34d79a39936152
Xavi Carrillo <xavi.carrillo@gmail.com>
2013-12-05 17:07:44 +0100

    Use an array of NTP servers

--- modules/ntp/manifests/init.pp
+++ modules/ntp/manifests/init.pp
@@ -1,5 +1,5 @@
 
-class ntp ($servers = [ '0.centos.pool.ntp.org' ],) {
+class ntp ($servers = [ '0.centos.pool.ntp.org','1.centos.pool.ntp.org','2.centos.pool.ntp.org' ],) {
 
   case $::osfamily{
     'Debian':  {
